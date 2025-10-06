771aee2a8b1ba1256b0f4acdf9dbc6f34b730058
Tayyaba Shahid <tayyabashahid019@gmail.com>
2025-10-06 23:50:19 +0500

    fixed issues

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,3 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Footer from "../components/Footer";
 import "../styles/Dashboard.css";
 
 export default function Dashboard() {
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -20,7 +20,7 @@ export default function Signup() {
 
     function validatePassword(password) {
       // Minimum 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character
-      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=\[\]{}|;:'",.<>/?]).{8,}$/;
+      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=[\]{}|;:'",.<>/?]).{8,}$/;
       return passwordRegex.test(password);
     }
 
